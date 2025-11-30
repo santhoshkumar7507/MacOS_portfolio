2d6078589f1d582983d2ba28c9b4a8c6e2f8e52a
santhoshkumar172003 <ssksanthoshkumar143@gmail.com>
2025-11-30 16:27:00 +0530

    message

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,36 @@
+import dayjs from "dayjs";
+import { navIcons,navLinks } from "#constants";
+
+const Navbar = () => {
+  return (
+    <nav>
+        <div>
+            <img src="/images/logo.svg" alt="logo"/>
+            <p className='font-bold'>Santhosh Kumar's portfolio</p>
+            <ul>
+                {navLinks.map(({id,name})=>(
+                    <li key={id}>
+                        <p>{name}</p>
+                    </li>
+                ))}
+            </ul>
+        </div>
+        <div className="flex items-center gap-5">
+            <ul className="flex gap-4">
+                {navIcons.map(({ id, img })=>(
+                    <li key={id}>
+                        <img src={img} className="icon-hover w-6"
+                        alt={`icon-${id}`}/>
+                    </li>
+                ))}
+            </ul>
+            <time>{dayjs().format("ddd MMM D h:mm A")}</time>
+        </div>
+
+
+
+    </nav>
+  );
+};
+
+export default Navbar;
